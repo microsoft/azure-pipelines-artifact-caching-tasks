ca38e3f87f79a4ae4675270bd7ec4318086670d0
Ethan Dennis <erdennis13@gmail.com>
2019-03-18 07:59:10 -0700

    Catch permissions errors from downloading and uploading packages:

--- Tasks/Common/packaging-common/cache/universaldownload.ts
+++ Tasks/Common/packaging-common/cache/universaldownload.ts
@@ -8,91 +8,137 @@ import * as artifactToolRunner from "./ArtifactToolRunner";
 import * as artifactToolUtilities from "./ArtifactToolUtilities";
 import * as auth from "./Authentication";
 
-export async function run(artifactToolPath: string, hash: string, targetFolder: string): Promise<boolean> {
-    try {
-        // Get directory to publish
-        let downloadDir: string = targetFolder;
-        if (downloadDir.length < 1) {
-            tl.warning(tl.loc("Info_DownloadDirectoryNotFound"));
-            return;
-        }
-
-        let serviceUri: string;
-        let feedId: string;
-        let packageName: string;
-        let version: string;
-
-        // Feed Auth
-        let internalAuthInfo: auth.InternalAuthInfo;
-
-        let toolRunnerOptions = artifactToolRunner.getOptions();
-
-        // getting inputs
-        serviceUri = tl.getEndpointUrl("SYSTEMVSSCONNECTION", false);
-
-        feedId = tl.getInput("feedList");
-
-        // Getting package name from hash
-        const packageId = tl.getVariable('Build.DefinitionName')
-            .replace(/\s/g, "")
-            .substring(0,255)
-            .toLowerCase();
-            
-        const accessToken = pkgLocationUtils.getSystemAccessToken();
-
-        internalAuthInfo = new auth.InternalAuthInfo([], accessToken);
-
-        const feedUri = await pkgLocationUtils.getFeedUriFromBaseServiceUri(serviceUri, accessToken);
-        packageName = await artifactToolUtilities.getPackageNameFromId(feedUri, accessToken, feedId, packageId);
-
-        // Getting package version from hash
-        version = `1.0.0-${hash}`;
-
-        toolRunnerOptions.env.UNIVERSAL_DOWNLOAD_PAT = internalAuthInfo.accessToken;
-
-        tl.debug(tl.loc("Info_UsingArtifactToolDownload"));
-
-        const downloadOptions = {
-            artifactToolPath,
-            feedId,
-            accountUrl: serviceUri,
-            packageName,
-            packageVersion: version,
-        } as artifactToolRunner.IArtifactToolOptions;
-
-        downloadPackageUsingArtifactTool(downloadDir, downloadOptions, toolRunnerOptions);
-
-        console.log('artifact downloaded');
-        return true;
-    } catch (err) {
-        if (!err.message.includes("Can't find the package")) {
-            tl.error(err);
-        }
-
-        return false;
+export async function run(
+  artifactToolPath: string,
+  hash: string,
+  targetFolder: string
+): Promise<boolean> {
+  try {
+    // Get directory to publish
+    let downloadDir: string = targetFolder;
+    if (downloadDir.length < 1) {
+      tl.warning(tl.loc("Info_DownloadDirectoryNotFound"));
+      return;
     }
+
+    let serviceUri: string;
+    let feedId: string;
+    let packageName: string;
+    let version: string;
+
+    // Feed Auth
+    let internalAuthInfo: auth.InternalAuthInfo;
+
+    let toolRunnerOptions = artifactToolRunner.getOptions();
+
+    // getting inputs
+    serviceUri = tl.getEndpointUrl("SYSTEMVSSCONNECTION", false);
+
+    feedId = tl.getInput("feedList");
+
+    // Getting package name from hash
+    const packageId = tl
+      .getVariable("Build.DefinitionName")
+      .replace(/\s/g, "")
+      .substring(0, 255)
+      .toLowerCase();
+
+    const accessToken = pkgLocationUtils.getSystemAccessToken();
+
+    internalAuthInfo = new auth.InternalAuthInfo([], accessToken);
+
+    const feedUri = await pkgLocationUtils.getFeedUriFromBaseServiceUri(
+      serviceUri,
+      accessToken
+    );
+    packageName = await artifactToolUtilities.getPackageNameFromId(
+      feedUri,
+      accessToken,
+      feedId,
+      packageId
+    );
+
+    // Getting package version from hash
+    version = `1.0.0-${hash}`;
+
+    toolRunnerOptions.env.UNIVERSAL_DOWNLOAD_PAT = internalAuthInfo.accessToken;
+
+    tl.debug(tl.loc("Info_UsingArtifactToolDownload"));
+
+    const downloadOptions = {
+      artifactToolPath,
+      feedId,
+      accountUrl: serviceUri,
+      packageName,
+      packageVersion: version
+    } as artifactToolRunner.IArtifactToolOptions;
+
+    downloadPackageUsingArtifactTool(
+      downloadDir,
+      downloadOptions,
+      toolRunnerOptions
+    );
+
+    console.log("artifact downloaded");
+    return true;
+  } catch (err) {
+    // if (!err.message.includes("Can't find the package")) {
+    //   tl.error(err);
+    // }
+    console.log(error);
+    return false;
+  }
 }
 
-function downloadPackageUsingArtifactTool(downloadDir: string, options: artifactToolRunner.IArtifactToolOptions, execOptions: IExecOptions) {
+function downloadPackageUsingArtifactTool(
+  downloadDir: string,
+  options: artifactToolRunner.IArtifactToolOptions,
+  execOptions: IExecOptions
+) {
+  let command = new Array<string>();
 
-    let command = new Array<string>();
+  command.push(
+    "universal",
+    "download",
+    "--feed",
+    options.feedId,
+    "--service",
+    options.accountUrl,
+    "--package-name",
+    options.packageName,
+    "--package-version",
+    options.packageVersion,
+    "--path",
+    downloadDir,
+    "--patvar",
+    "UNIVERSAL_DOWNLOAD_PAT",
+    "--verbosity",
+    tl.getInput("verbosity")
+  );
 
-    command.push("universal", "download",
-        "--feed", options.feedId,
-        "--service", options.accountUrl,
-        "--package-name", options.packageName,
-        "--package-version", options.packageVersion,
-        "--path", downloadDir,
-        "--patvar", "UNIVERSAL_DOWNLOAD_PAT",
-        "--verbosity", tl.getInput("verbosity"));
+  console.log(
+    tl.loc(
+      "Info_Downloading",
+      options.packageName,
+      options.packageVersion,
+      options.feedId
+    )
+  );
+  const execResult: IExecSyncResult = artifactToolRunner.runArtifactTool(
+    options.artifactToolPath,
+    command,
+    execOptions
+  );
+  if (execResult.code === 0) {
+    return;
+  }
 
-    console.log(tl.loc("Info_Downloading", options.packageName, options.packageVersion, options.feedId));
-    const execResult: IExecSyncResult = artifactToolRunner.runArtifactTool(options.artifactToolPath, command, execOptions);
-    if (execResult.code === 0) {
-        return;
-    }
-
-    throw new Error(tl.loc("Error_UnexpectedErrorArtifactToolDownload",
-        execResult.code,
-        execResult.stderr ? execResult.stderr.trim() : execResult.stderr));
+  // TODO: Check if permissions exist
+  console.log(
+    tl.loc(
+      "Error_UnexpectedErrorArtifactToolDownload",
+      execResult.code,
+      execResult.stderr ? execResult.stderr.trim() : execResult.stderr
+    )
+  );
 }
--- Tasks/Common/packaging-common/cache/universalpublish.ts
+++ Tasks/Common/packaging-common/cache/universalpublish.ts
@@ -9,119 +9,170 @@ import * as artifactToolRunner from "./ArtifactToolRunner";
 import * as artifactToolUtilities from "./ArtifactToolUtilities";
 import * as auth from "./Authentication";
 
-export async function run(artifactToolPath: string, hash: string, targetFolder: string): Promise<boolean> {
-    let buildIdentityDisplayName: string = null;
-    let buildIdentityAccount: string = null;
-    try {
-        // Get directory to publish
-        let publishDir: string = targetFolder;
-        let serviceUri: string;
-        let feedId: string;
-        let packageName: string;
-        let version: string = `1.0.0-${hash}`;
-        let accessToken: string;
-        let feedUri: string;
-        let publishedPackageVar: string = tl.getInput("publishedPackageVar");
-        const versionRadio = 'custom';
+export async function run(
+  artifactToolPath: string,
+  hash: string,
+  targetFolder: string
+): Promise<boolean> {
+  let buildIdentityDisplayName: string = null;
+  let buildIdentityAccount: string = null;
+  try {
+    // Get directory to publish
+    let publishDir: string = targetFolder;
+    let serviceUri: string;
+    let feedId: string;
+    let packageName: string;
+    let version: string = `1.0.0-${hash}`;
+    let accessToken: string;
+    let feedUri: string;
+    let publishedPackageVar: string = tl.getInput("publishedPackageVar");
+    const versionRadio = "custom";
 
-        let internalAuthInfo: auth.InternalAuthInfo;
+    let internalAuthInfo: auth.InternalAuthInfo;
 
-        let toolRunnerOptions = artifactToolRunner.getOptions();
+    let toolRunnerOptions = artifactToolRunner.getOptions();
 
-        let sessionId: string;
+    let sessionId: string;
 
-        // getting inputs
-        serviceUri = tl.getEndpointUrl("SYSTEMVSSCONNECTION", false);
+    // getting inputs
+    serviceUri = tl.getEndpointUrl("SYSTEMVSSCONNECTION", false);
 
-        packageName = tl.getVariable('Build.DefinitionName')
-            .replace(/\s/g, "")
-            .substring(0,255)
-            .toLowerCase();
+    packageName = tl
+      .getVariable("Build.DefinitionName")
+      .replace(/\s/g, "")
+      .substring(0, 255)
+      .toLowerCase();
 
-        feedId = tl.getInput("feedList");
+    feedId = tl.getInput("feedList");
 
-        // Setting up auth info
-        accessToken = pkgLocationUtils.getSystemAccessToken();
-        internalAuthInfo = new auth.InternalAuthInfo([], accessToken);
+    // Setting up auth info
+    accessToken = pkgLocationUtils.getSystemAccessToken();
+    internalAuthInfo = new auth.InternalAuthInfo([], accessToken);
 
-        toolRunnerOptions.env.UNIVERSAL_PUBLISH_PAT = internalAuthInfo.accessToken;
+    toolRunnerOptions.env.UNIVERSAL_PUBLISH_PAT = internalAuthInfo.accessToken;
 
-        // creating session
-        const useSessionEnabled = tl.getVariable("Packaging.SavePublishMetadata");
-        if (useSessionEnabled) {
-            let packagingLocation: string;
-            try {
-                // This call is to get the packaging URI(abc.pkgs.vs.com) which is same for all protocols.
-                packagingLocation = await pkgLocationUtils.getNuGetUriFromBaseServiceUri(
-                    serviceUri,
-                    accessToken);
-            } catch (error) {
-                tl.debug(JSON.stringify(error));
-                packagingLocation = serviceUri;
-            }
+    // creating session
+    const useSessionEnabled = tl.getVariable("Packaging.SavePublishMetadata");
+    if (useSessionEnabled) {
+      let packagingLocation: string;
+      try {
+        // This call is to get the packaging URI(abc.pkgs.vs.com) which is same for all protocols.
+        packagingLocation = await pkgLocationUtils.getNuGetUriFromBaseServiceUri(
+          serviceUri,
+          accessToken
+        );
+      } catch (error) {
+        tl.debug(JSON.stringify(error));
+        packagingLocation = serviceUri;
+      }
 
-            const pkgConn = pkgLocationUtils.getWebApiWithProxy(packagingLocation, accessToken);
-            sessionId = await ProvenanceHelper.GetSessionId(
-                feedId,
-                "upack", /* must match protocol name on the server */
-                pkgConn.serverUrl,
-                [pkgConn.authHandler],
-                pkgConn.options);
-        }
-
-        tl.debug(tl.loc("Info_UsingArtifactToolPublish"));
-
-        if (sessionId != null) {
-            feedId = sessionId;
-        }
-
-        // tslint:disable-next-line:no-object-literal-type-assertion
-        const publishOptions = {
-            artifactToolPath,
-            feedId,
-            accountUrl: serviceUri,
-            packageName,
-            packageVersion: version,
-        } as artifactToolRunner.IArtifactToolOptions;
-
-        publishPackageUsingArtifactTool(publishDir, publishOptions, toolRunnerOptions);
-        if (publishedPackageVar) {
-            tl.setVariable(publishedPackageVar, `${packageName} ${version}`);
-        }
-
-        return true;
-    } catch (err) {
-        tl.error(err);
-
-        if (buildIdentityDisplayName || buildIdentityAccount) {
-            tl.warning(tl.loc("BuildIdentityPermissionsHint", buildIdentityDisplayName, buildIdentityAccount));
-        }
-
-        tl.setResult(tl.TaskResult.Failed, tl.loc("PackagesFailedToPublish"));
-        return false;
+      const pkgConn = pkgLocationUtils.getWebApiWithProxy(
+        packagingLocation,
+        accessToken
+      );
+      sessionId = await ProvenanceHelper.GetSessionId(
+        feedId,
+        "upack" /* must match protocol name on the server */,
+        pkgConn.serverUrl,
+        [pkgConn.authHandler],
+        pkgConn.options
+      );
     }
+
+    tl.debug(tl.loc("Info_UsingArtifactToolPublish"));
+
+    if (sessionId != null) {
+      feedId = sessionId;
+    }
+
+    // tslint:disable-next-line:no-object-literal-type-assertion
+    const publishOptions = {
+      artifactToolPath,
+      feedId,
+      accountUrl: serviceUri,
+      packageName,
+      packageVersion: version
+    } as artifactToolRunner.IArtifactToolOptions;
+
+    publishPackageUsingArtifactTool(
+      publishDir,
+      publishOptions,
+      toolRunnerOptions
+    );
+    if (publishedPackageVar) {
+      tl.setVariable(publishedPackageVar, `${packageName} ${version}`);
+    }
+
+    return true;
+  } catch (err) {
+    tl.error(err);
+
+    if (buildIdentityDisplayName || buildIdentityAccount) {
+      tl.warning(
+        tl.loc(
+          "BuildIdentityPermissionsHint",
+          buildIdentityDisplayName,
+          buildIdentityAccount
+        )
+      );
+    }
+
+    // tl.setResult(tl.TaskResult.Failed, tl.loc("PackagesFailedToPublish"));
+    return false;
+  }
 }
 
-function publishPackageUsingArtifactTool(publishDir: string, options: artifactToolRunner.IArtifactToolOptions, execOptions: IExecOptions) {
-    let command = new Array<string>();
-    command.push("universal", "publish",
-        "--feed", options.feedId,
-        "--service", options.accountUrl,
-        "--package-name", options.packageName,
-        "--package-version", options.packageVersion,
-        "--path", publishDir,
-        "--patvar", "UNIVERSAL_PUBLISH_PAT",
-        "--verbosity", tl.getInput("verbosity"),
-        "--description", tl.getInput("packagePublishDescription"));
+function publishPackageUsingArtifactTool(
+  publishDir: string,
+  options: artifactToolRunner.IArtifactToolOptions,
+  execOptions: IExecOptions
+) {
+  let command = new Array<string>();
+  command.push(
+    "universal",
+    "publish",
+    "--feed",
+    options.feedId,
+    "--service",
+    options.accountUrl,
+    "--package-name",
+    options.packageName,
+    "--package-version",
+    options.packageVersion,
+    "--path",
+    publishDir,
+    "--patvar",
+    "UNIVERSAL_PUBLISH_PAT",
+    "--verbosity",
+    tl.getInput("verbosity"),
+    "--description",
+    tl.getInput("packagePublishDescription")
+  );
 
-    console.log(tl.loc("Info_Publishing", options.packageName, options.packageVersion, options.feedId));
-    const execResult: IExecSyncResult = artifactToolRunner.runArtifactTool(options.artifactToolPath, command, execOptions);
+  console.log(
+    tl.loc(
+      "Info_Publishing",
+      options.packageName,
+      options.packageVersion,
+      options.feedId
+    )
+  );
+  const execResult: IExecSyncResult = artifactToolRunner.runArtifactTool(
+    options.artifactToolPath,
+    command,
+    execOptions
+  );
 
-    if (execResult.code === 0) {
-        return;
-    }
+  if (execResult.code === 0) {
+    return;
+  }
 
-    throw new Error(tl.loc("Error_UnexpectedErrorArtifactTool",
-        execResult.code,
-        execResult.stderr ? execResult.stderr.trim() : execResult.stderr));
+  // TODO: Check if permissions exist
+  console.log(
+    tl.loc(
+      "Error_UnexpectedErrorArtifactTool",
+      execResult.code,
+      execResult.stderr ? execResult.stderr.trim() : execResult.stderr
+    )
+  );
 }
